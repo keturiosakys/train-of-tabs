7a6a0779bfe3355cbc2a837007b51c436dc09b1b
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-02-07 09:18:59 +0100

    load tabTree when installed so the extension is immediately useful

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,5 @@
 import { TabNode } from "./types";
-import { mapToObject } from "./utils";
-
-export const tabTree: Map<number, TabNode> = new Map();
+import { mapToObject, objectToMap } from "./utils";
 
 function createTabNode(tab: chrome.tabs.Tab): TabNode | undefined {
   if (!tab.id) {
@@ -19,9 +17,60 @@ function createTabNode(tab: chrome.tabs.Tab): TabNode | undefined {
   };
 }
 
-chrome.runtime.onInstalled.addListener(() => {
+const tabTree: Map<number, TabNode> = new Map();
+
+chrome.runtime.onInstalled.addListener(async () => {
+  try {
+    // set up the initial tabTree
+
+    const windows = await chrome.windows.getAll();
+
+    for (const window of windows) {
+      const tabs = await chrome.tabs.query({ windowId: window.id });
+      for (const tab of tabs) {
+        const currentTabNode = createTabNode(tab);
+
+        if (currentTabNode && tab.id) {
+          tabTree.set(tab.id, currentTabNode);
+
+          if (tab.openerTabId) {
+            const originTabNode = tabTree.get(tab.openerTabId);
+            if (originTabNode) {
+              // @ts-ignore - if the originTabNode exists, it will have children []
+              originTabNode.children.push(currentTabNode);
+              tabTree.set(tab.openerTabId, originTabNode);
+            }
+          }
+        }
+      }
+    }
+
+    await chrome.storage.session.set({ tabTree: mapToObject(tabTree) });
+  } catch (e) {
+    console.error(e);
+  }
   console.log("Train o'tabs extension installed. Choo choo...");
 });
+
+// chrome.tabs.onActivated.addListener(async (tabInfo) => {
+//   const tab = await chrome.tabs.get(tabInfo.tabId);
+//   if (!tab.id) {
+//     return;
+//   }
+//   const activeTabNode: TabNode = {
+//     tabId: tab?.id,
+//     url: tab?.url || "",
+//     title: tab?.title || "",
+//     favicon: tab?.favIconUrl || "",
+//     originTabId: tab?.openerTabId,
+//     status: "open",
+//   };
+//
+//   const tabTree = objectToMap(await chrome.storage.session.get("tabTree"));
+//
+//   tabTree.set(tab.id, activeTabNode);
+//   chrome.storage.session.set({ tabTree: mapToObject(tabTree) });
+// });
 
 chrome.tabs.onCreated.addListener(async (tab) => {
   if (!tab.id) {
@@ -31,7 +80,7 @@ chrome.tabs.onCreated.addListener(async (tab) => {
   const createdTabNode: TabNode = {
     tabId: tab.id,
     url: tab.url || "",
-		favicon: tab.favIconUrl || "",
+    favicon: tab.favIconUrl || "",
     title: tab.title || "",
     originTabId: tab.openerTabId,
     status: "open",
@@ -53,6 +102,7 @@ chrome.tabs.onCreated.addListener(async (tab) => {
   }
 
   chrome.storage.session.set({ tabTree: mapToObject(tabTree) });
+  console.log("tabTree updated");
 });
 
 chrome.tabs.onUpdated.addListener(async (tabId, _changeInfo, tab) => {
@@ -61,7 +111,7 @@ chrome.tabs.onUpdated.addListener(async (tabId, _changeInfo, tab) => {
   if (tabNode) {
     tabNode.url = tab.url || "";
     tabNode.title = tab.title || "";
-		tabNode.favicon = tab.favIconUrl || "";
+    tabNode.favicon = tab.favIconUrl || "";
     tabNode.originTabId = tab.openerTabId;
     tabTree.set(tabId, tabNode);
   }
@@ -69,7 +119,7 @@ chrome.tabs.onUpdated.addListener(async (tabId, _changeInfo, tab) => {
   chrome.storage.session.set({ tabTree: mapToObject(tabTree) });
 });
 
-chrome.tabs.onRemoved.addListener((tabId) => {
+chrome.tabs.onRemoved.addListener(async (tabId) => {
   const tabNode = tabTree.get(tabId);
 
   if (tabNode) {
