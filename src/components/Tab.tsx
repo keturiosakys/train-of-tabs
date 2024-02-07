3ad9b289452187e69281d6636b6548aa2873b431
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-02-07 09:18:43 +0100

    modify CurrentTab

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,5 +1,4 @@
 import { TabNode } from "../types";
-import { autofocus } from "@solid-primitives/autofocus";
 import { setUpdated } from "./Train";
 
 function handleTabClick(id: number) {
@@ -10,11 +9,14 @@ function handleTabClick(id: number) {
 export function CurrentTab(props: { tab: TabNode | undefined }) {
 	const { tab } = props;
 	return (
-		<div
-			class="rounded-lg group border-dashed border-2 border-gray-500 bg-gray-900 opacity-75 py-2 px-4"
-		>
+		<div class="rounded-xl group bg-gray-300 dark:bg-gray-600 opacity-75 py-2 px-4">
 			<li class="text-left flex">
-				<img class="w-4 h-4 align-middle mr-2" style="position: relative; top: 1px;" src={tab?.favicon} alt="tab favicon" />
+				<img
+					class="w-4 h-4 align-middle mr-2"
+					style="position: relative; top: 1px;"
+					src={tab?.favicon}
+					alt="tab favicon"
+				/>
 				<p class="truncate whitespace-nowrap">{tab?.title}</p>
 			</li>
 		</div>
@@ -28,13 +30,17 @@ export function Tab(props: { tab: TabNode | undefined }) {
 	}
 	return (
 		<button
-			class="rounded-lg group hover:bg-gray-600 focus-visible:bg-gray-200 dark:hover:bg-gray-600 dark:focus-visible:bg-gray-600 transition-colors opacity-75 py-2 px-4"
+			class="rounded-xl group hover:bg-gray-200 focus-visible:bg-gray-200 dark:hover:bg-gray-600 dark:focus-visible:bg-gray-600 transition-colors opacity-75 py-2 px-4"
 			type="button"
-			autofocus
 			onClick={() => handleTabClick(tab?.tabId as number)}
 		>
 			<li class="text-left flex">
-				<img class="w-4 h-4 align-middle mr-2" style="position: relative; top: 1px;" src={tab?.favicon} alt="tab favicon" />
+				<img
+					class="w-4 h-4 align-middle mr-2"
+					style="position: relative; top: 1px;"
+					src={tab?.favicon}
+					alt="tab favicon"
+				/>
 				<p class="truncate whitespace-nowrap">{tab?.title}</p>
 			</li>
 		</button>
