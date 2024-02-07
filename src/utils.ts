604b2d0c1c1f20cd4bd871148da54881400b4d6c
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-02-07 09:18:10 +0100

    formatting

--- src/utils.ts
+++ src/utils.ts
@@ -1,34 +1,35 @@
 import { TabNode } from "./types";
 
-export async function getCurrentTab() {
-	const [tab] = await chrome.tabs.query({
-		active: true,
-		currentWindow: true,
-	});
+export async function getCurrentTab(): Promise<chrome.tabs.Tab | undefined> {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
 
-	return tab;
+  return tab;
 }
+
 
 // Convert Map to plain object before setting it to session storage
 export function mapToObject(
-	map: Map<number, TabNode>,
+  map: Map<number, TabNode>,
 ): Record<number, TabNode> {
-	const obj: Record<number, TabNode> = {};
-	for (const [key, value] of map.entries()) {
-		obj[key] = value;
-	}
-	return obj;
+  const obj: Record<number, TabNode> = {};
+  for (const [key, value] of map.entries()) {
+    obj[key] = value;
+  }
+  return obj;
 }
 
 // Convert plain object back to Map after getting it from session storage
 export function objectToMap(
-	obj: Record<number, TabNode>,
+  obj: Record<number, TabNode>,
 ): Map<number, TabNode> {
-	const map = new Map<number, TabNode>();
-	for (const key in obj) {
-		if (Object.prototype.hasOwnProperty.call(obj, key)) {
-			map.set(Number(key), obj[key]);
-		}
-	}
-	return map;
+  const map = new Map<number, TabNode>();
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      map.set(Number(key), obj[key]);
+    }
+  }
+  return map;
 }
